a9e2c71ee5ec28736aa7979c530eadaccde1d153
rinaldi <darksideofbotix@gmail.com>
2021-06-17 04:10:03 +0700

    First Commit

---
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<h1>Antariksa</h1>
---
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -0,0 +1,16 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    <div>
+        <h1>Antariksa</h1>
+    </div>
+
+    <div class="mt-8 text-2xl">
+        Selamat Datang di Aplikasi Antariksa!
+    </div>
+
+    <div class="mt-6 text-gray-500">
+        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
+        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
+        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
+        ecosystem to be a breath of fresh air. We hope you love it.
+    </div>
+</div>
